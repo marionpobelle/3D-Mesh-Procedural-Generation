b0d68d10e7f47ba9a8d6518718347585452995be
marionpobelle <marion.pobelle@hotmail.com>
2022-10-24 23:00:20 +0200

    Added endless terrain generation

--- Assets/Editor/MapGeneratorEditor.cs
+++ Assets/Editor/MapGeneratorEditor.cs
@@ -9,10 +9,10 @@ public class MapGeneratorEditor : Editor
     public override void OnInspectorGUI(){
         MapGenerator mapGen = (MapGenerator)target;
         if(DrawDefaultInspector()){
-            if(mapGen.autoUpdate) mapGen.GenerateMap();
+            if(mapGen.autoUpdate) mapGen.DrawMapInEditor();
         }
         if(GUILayout.Button("Generate")){
-            mapGen.GenerateMap();
+            mapGen.DrawMapInEditor();
         }
     }
 }
