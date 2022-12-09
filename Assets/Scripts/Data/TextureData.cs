9708306114ab673b41430671e42cf52a3aff4124
marionpobelle <marion.pobelle@hotmail.com>
2022-12-09 16:31:39 +0100

    Switched to Shader Graph

---
+++ Assets/Scripts/Data/TextureData.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Linq;
+
+[CreateAssetMenu()]
+public class TextureData : UpdatableData {
+
+	float savedMinHeight;
+	float savedMaxHeight;
+
+	public void ApplyToMaterial(Material material) {
+
+		UpdateMeshHeights (material, savedMinHeight, savedMaxHeight);
+	}
+
+	public void UpdateMeshHeights(Material material, float minHeight, float maxHeight) {
+		savedMinHeight = minHeight;
+		savedMaxHeight = maxHeight;
+
+		material.SetFloat ("minHeight", minHeight);
+		material.SetFloat ("maxHeight", maxHeight);
+	}
+}
+
